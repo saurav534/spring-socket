ab584c7b2824fcd50a27f3c4e9b03f5ede8850c2
sauravsingh <singh.saurav@rivigo.com>
2018-12-23 18:08:37 +0530

    project init

---
+++ src/main/java/com/saurav/socketexample/SocketExampleApplication.java
@@ -0,0 +1,14 @@
+package com.saurav.socketexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketExampleApplication.class, args);
+	}
+
+}
+
